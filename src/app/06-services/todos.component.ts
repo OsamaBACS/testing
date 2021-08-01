87f0e995256455928c5f47879fc51c5b66d2c725
OsamaBACS <osama.solution@gmail.com>
2021-08-01 22:33:42 +0300

    initialCommit

---
+++ src/app/06-services/todos.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { TodoService } from './todo.service';
+
+@Component({
+  selector: '',
+  template: '',
+  styleUrls: [],
+})
+export class TodosComponent {
+  todos: any[] = [];
+  message: any;
+
+  constructor(private service: TodoService) {}
+
+  ngOnInit() {
+    this.service.getTodos().subscribe((t) => (this.todos = t));
+  }
+
+  add() {
+    var newTodo = { title: '... ' };
+    this.service.add(newTodo).subscribe(
+      (t) => this.todos.push(t),
+      (err) => (this.message = err)
+    );
+  }
+
+  delete(id: any) {
+    if (confirm('Are you sure?')) this.service.delete(id).subscribe();
+  }
+}
